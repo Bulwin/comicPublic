93a93905fdd4ab039783ac34d0a141ee41e3e3b8
Bulwin <nikoz@mail.ru>
2026-01-20 13:31:07 +0200

    fix: экранированы фигурные скобки в simple_image_prompt.txt для .format()

---
+++ check_logs.py
@@ -0,0 +1,25 @@
+import paramiko
+import sys
+
+# Устанавливаем кодировку для вывода
+sys.stdout.reconfigure(encoding='utf-8')
+
+host = '95.216.138.177'
+user = 'root'
+password = '93btksANXFgV'
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, username=user, password=password, timeout=30)
+
+# Перезапустить и посмотреть логи
+cmd = 'systemctl restart dailycomicbot && sleep 3 && journalctl -u dailycomicbot -n 10 --no-pager'
+print(f'>>> {cmd}')
+stdin, stdout, stderr = client.exec_command(cmd, timeout=60)
+output = stdout.read().decode('utf-8', errors='ignore')
+print(output)
+err = stderr.read().decode('utf-8', errors='ignore')
+if err:
+    print(f'STDERR: {err}')
+
+client.close()
